e04055fbf3721334dff7c87143d58a77d6c71493
Sara Young <sarakitchenyoung@gmail.com>
2020-08-05 10:36:12 -0600

    tranferred code into methods and specified play again responses

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -12,6 +12,20 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def get_user_input
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}.")
+    prompt("(Optional: Type 'r' for rock, 'p' for paper, " \
+     "'sc' for scissors, 'l' for lizard, 'sp' for spock.)")
+    choice = gets.chomp.downcase
+    choice = convert_choice(choice)
+
+    return choice if VALID_CHOICES.include?(choice)
+    prompt("That's not a valid choice. Try again.")
+  end
+end
+
 def convert_choice(entry)
   case entry
   when 'r' then 'rock'
@@ -63,24 +77,32 @@ def display_winner(scores)
   end
 end
 
+def match_over?(scores)
+  if scores[:player] == WINNING_SCORE || scores[:computer] == WINNING_SCORE
+    display_winner(scores)
+    true
+  end
+end
+
+def play_again?
+  loop do
+    prompt("Would you like to play again? (Y/N)")
+    answer = gets.chomp.downcase
+    if answer == 'y'
+      return true
+    elsif answer == 'n'
+      return false
+    end
+  end
+end
+
 loop do
   scores = { player: 0, computer: 0 }
   round_count = 0
-  prompt('Win 5 rounds to beat the game!')
+  prompt("Win #{WINNING_SCORE} rounds to beat the game!")
   loop do
-    choice = ''
     round_count += 1
-    loop do
-      prompt("Choose one: #{VALID_CHOICES.join(', ')}.")
-      prompt("(Optional: Type 'r' for rock, 'p' for paper, " \
-       "'sc' for scissors, 'l' for lizard, 'sp' for spock.)")
-
-      choice = gets.chomp.downcase
-      choice = convert_choice(choice)
-
-      break if VALID_CHOICES.include?(choice)
-      prompt("That's not a valid choice. Try again.")
-    end
+    choice = get_user_input
 
     computer_choice = VALID_CHOICES.sample
 
@@ -90,20 +112,13 @@ loop do
       "Computer chose #{computer_choice}")
 
     display_result(choice, computer_choice)
-
     calculate_score(choice, computer_choice, scores)
-
     display_score(scores)
 
-    if scores[:player] == WINNING_SCORE || scores[:computer] == WINNING_SCORE
-      display_winner(scores)
-      break
-    end
+    break if match_over?(scores)
   end
 
-  prompt("Would you like to play again?")
-  answer = gets.chomp
-  break unless answer.downcase == 'yes' || answer.downcase == 'y'
+  break unless play_again?
 end
 
 prompt('Thank you for playing. Goodbye!')
